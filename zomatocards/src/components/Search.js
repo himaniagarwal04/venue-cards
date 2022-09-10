d1bbb058d06e6f1fdeb269cded40b1e752d11e4c
Himani <h.agarwal46@outlook.com>
2022-09-10 10:50:06 +0530

    fooddata

--- zomatocards/src/components/Search.js
+++ zomatocards/src/components/Search.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Fooddata from './FoodData'
 import "./style.css"
+
+
 const Search = () => {
+
+    const [fdata, setFdata] = useState(Fooddata);
+    // console.log(fdata);
   return (
     <div>Search</div>
   )
